ae356ad2d0b0cd5b343e04b83060813f180d7727
Tomasz Trzaska <tommy.trzaska@gmail.com>
2014-12-30 16:09:02 +0100

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public int minutes = 2;
+	public int seconds = 0;
+	public Text timeText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		timeText = GetComponent<Text> ();
+		StartCoroutine ("CountDown");
+	}
+
+	IEnumerator CountDown ()
+	{
+		while (true)
+		{
+			if(seconds==0)
+			{
+				minutes--;
+				seconds = 60;
+			}
+			seconds--;
+			if(minutes==0)
+			{
+				if(seconds>10)
+					timeText.color = Color.yellow;
+				else if(seconds>0)
+					timeText.color = Color.red;
+			}
+			timeText.text = minutes+":"+seconds.ToString ("D2");
+			yield return new WaitForSeconds (1);
+
+			if(minutes==0 && seconds==0)
+			{
+				ResetTimer ();
+				GameManager.instance.NextPlayer ();
+			}
+		}
+	}
+
+	public void ResetTimer ()
+	{
+		minutes = 2;
+		seconds = 0;
+		timeText.color = Color.green;
+		//GameManager.instance.NextPlayer ();
+	}
+}
